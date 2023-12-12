27a709832513333acfd85858ea9c74e4461c86f2
Siarhei_Kakichau <siarhei_kakichau@epam.com>
2023-12-12 14:39:34 +0300

    base logging

--- file-garbage-core/src/main/java/org/student/CoreApplication.java
+++ file-garbage-core/src/main/java/org/student/CoreApplication.java
@@ -1,7 +1,17 @@
 package org.student;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@SpringBootApplication
 public class CoreApplication {
+
+	private static final Logger logger = LogManager.getLogger(CoreApplication.class);
+
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		SpringApplication.run(CoreApplication.class, args);
+		logger.info("[CORE MODULE] started.");
 	}
 }
