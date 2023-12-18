318da303dab3d35500c3a5c71c841e62e8cf8088
Siarhei Kakichev <57998973+Kasean@users.noreply.github.com>
2023-12-18 18:48:17 +0300

    Update SecurityConfig.java

--- file-garbage-core/src/main/java/org/student/auth/SecurityConfig.java
+++ file-garbage-core/src/main/java/org/student/auth/SecurityConfig.java
@@ -1,46 +1,36 @@
 package org.student.auth;
 
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@EnableWebFluxSecurity
-@EnableMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
+@Configuration
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/super_admin/**").hasAuthority("SCOPE_SUPERADMIN")
-                        .pathMatchers("/admin/**").hasAuthority("SCOPE_ADMIN")
-                        .anyExchange().authenticated())
-                .oauth2Client(withDefaults())
-                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
-                        .jwt(withDefaults()));
-        return http.build();
+    @ConditionalOnMissingBean(SessionAuthenticationStrategy.class)
+    public SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
     @Bean
-    public OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
-        final OidcUserService delegate = new OidcUserService();
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
 
-        return (userRequest) -> {
-            OidcUser oidcUser = delegate.loadUser(userRequest);
-
-            final Map<String, Object> claims = oidcUser.getClaims();
-            final JSONArray groups = (JSONArray) claims.get("groups");
-
-            final Set<GrantedAuthority> mappedAuthorities = groups.stream()
-                    .map(role -> new SimpleGrantedAuthority(("ROLE_" + role)))
-                    .collect(Collectors.toSet());
-
-            return new DefaultOidcUser(mappedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
-        };
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers("/secured/*").authenticated();
     }
 }
