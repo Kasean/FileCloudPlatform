a80343b631a5262000e1e1519cf4ebaddfb2003d
kasean <serguei.kakichev@mail.ru>
2023-12-18 18:07:46 +0300

    fix errors

--- README.md
+++ README.md
@@ -7,12 +7,8 @@
 1. core application - 8080
 2. keycloak - 9990
 
-errors 
+2023-12-18T17:58:59.628+03:00  WARN 47464 --- [           main] .s.s.UserDetailsServiceAutoConfiguration :
 
-docker-keycloak-1: 14:37:24,942 INFO  [org.jboss.vfs] (MSC service thread 1-8) VFS000002: Failed to clean existing content for temp file provider of type temp. Enable DEBUG level log to find what caused this
-docker-keycloak-1: WARNING: An illegal reflective access operation has occurred
-docker-keycloak-1: WARNING: Illegal reflective access by org.wildfly.extension.elytron.SSLDefinitions (jar:file:/opt/jboss/keycloak/modules/system/layers/base/org/wildfly/extension/elytron/main/wildfly-elytron-integration-18.0.4.Final.jar!/) to method com.sun.net.ssl.internal.ssl.Provider.isFIPS()
-docker-keycloak-1: WARNING: Please consider reporting this to the maintainers ofdocker-keycloak-1:  org.wildfly.extension.elytron.SSLDefinitions
-docker-keycloak-1: WARNING: Use --illegal-access=warn to enable warnings of further illegal reflective access operations
-docker-keycloak-1: WARNING: All illegal access operations will be denied in a future release
-docker-keycloak-1: 14:37:26,265 INFO  [org.wildfly.security] (ServerService Thread Pool -- 21) ELY00001: WildFly Elytron version 1.18.3.Final
+Using generated security password: 897a4b2e-a296-4c36-9b84-024b29cc3289
+
+This generated password is for development use only. Your security configuration must be updated before running your application in production.
--- docker/create_users_and_roles.sh
+++ docker/create_users_and_roles.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-# Start Keycloak
-/opt/jboss/tools/docker-entrypoint.sh -b 0.0.0.0 &
+set -x
 
-# Wait for Keycloak to start
-sleep 30
+# Start Keycloak
+#/opt/jboss/tools/docker-entrypoint.sh -b 0.0.0.0 &
+#
+## Wait for Keycloak to start
+#sleep 30
 
 # Get admin access token
-TOKEN=$(curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'username=admin' -d 'password=admin' -d 'grant_type=password' -d 'client_id=admin-cli' "http://localhost:8080/auth/realms/master/protocol/openid-connect/token" | jq -r .access_token)
+TOKEN=$(curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'username=admin' -d 'password=admin' -d 'grant_type=password' -d 'client_id=admin-cli' "http://localhost:9990/auth/realms/master/protocol/openid-connect/token" | jq -r .access_token)
 
 # Create roles
-curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"name":"SUPERADMIN"}' "http://localhost:8080/auth/admin/realms/master/roles"
-curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"name":"ADMIN"}' "http://localhost:8080/auth/admin/realms/master/roles"
+curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"name":"SUPERADMIN"}' "http://localhost:9990/auth/admin/realms/master/roles"
+curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"name":"ADMIN"}' "http://localhost:9990/auth/admin/realms/master/roles"
 
 # Create client
-curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"clientId":"CoreApplication", "secret":"SecretCoreApplication", "enabled":true, "publicClient":false, "redirectUris":["/*"]}' "http://localhost:8080/auth/admin/realms/master/clients"
+curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"clientId":"CoreApplication", "secret":"SecretCoreApplication", "enabled":true, "publicClient":false, "redirectUris":["/*"]}' "http://localhost:9990/auth/admin/realms/master/clients"
 
 # Create users
-curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"username":"SUPERADMIN","enabled":true,"credentials":[{"type":"password","value":"superadminpassword","temporary":false}]}' "http://localhost:8080/auth/admin/realms/master/users"
-curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"username":"ADMIN","enabled":true,"credentials":[{"type":"password","value":"adminpassword","temporary":false}]}' "http://localhost:8080/auth/admin/realms/master/users"
+curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"username":"SUPERADMIN","enabled":true,"credentials":[{"type":"password","value":"superadminpassword","temporary":false}]}' "http://localhost:9990/auth/admin/realms/master/users"
+curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"username":"ADMIN","enabled":true,"credentials":[{"type":"password","value":"adminpassword","temporary":false}]}' "http://localhost:9990/auth/admin/realms/master/users"
 
 # Assign roles to users
-SUPERADMIN_ID=$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "http://localhost:8080/auth/admin/realms/master/users?username=SUPERADMIN" | jq -r .[0].id)
-ADMIN_ID=$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "http://localhost:8080/auth/admin/realms/master/users?username=ADMIN" | jq -r .[0].id)
+SUPERADMIN_ID=$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "http://localhost:9990/auth/admin/realms/master/users?username=SUPERADMIN" | jq -r .[0].id)
+ADMIN_ID=$(curl -s -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" "http://localhost:9990/auth/admin/realms/master/users?username=ADMIN" | jq -r .[0].id)
 
-curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '[{"id":"","name":"SUPERADMIN"}]' "http://localhost:8080/auth/admin/realms/master/users/$SUPERADMIN_ID/role-mappings/realm"
-curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '[{"id":"","name":"ADMIN"}]' "http://localhost:8080/auth/admin/realms/master/users/$ADMIN_ID/role-mappings/realm"
+curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '[{"id":"","name":"SUPERADMIN"}]' "http://localhost:9990/auth/admin/realms/master/users/$SUPERADMIN_ID/role-mappings/realm"
+curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '[{"id":"","name":"ADMIN"}]' "http://localhost:9990/auth/admin/realms/master/users/$ADMIN_ID/role-mappings/realm"
 
 tail -f /dev/null
