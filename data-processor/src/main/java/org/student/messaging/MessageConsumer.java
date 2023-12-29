97a1356853148ef38df8050335706c2ae19ebf7a
kasean <serguei.kakichev@mail.ru>
2023-12-29 14:50:28 +0300

    data processor messaging #11

---
+++ data-processor/src/main/java/org/student/messaging/MessageConsumer.java
@@ -0,0 +1,13 @@
+package org.student.messaging;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageConsumer {
+    @KafkaListener(id = "data-processor-listener", topicPartitions = @TopicPartition(topic = "artifact-processor-topic", partitions = {"0"}), groupId = "core-file-garbage-group")
+    public void consume(byte[] message) {
+        System.out.println("Consumed message: " + new String(message));
+    }
+}
